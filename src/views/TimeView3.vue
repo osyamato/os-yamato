add1c4467c8665bca82d8a084de7ed3e0d235a1d
Kuraoka <support-weather@hanaco875.com>
2025-07-02 00:45:53 +0000

    🕰️ Add clock parent view with switch panel (TimeView0) and integrate TimeView2 & TimeView3

---
+++ src/views/TimeView3.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="time3-container">
+    <h2>TimeView3</h2>
+    <p>ここに新しい時計のデザインを追加していきます！</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.time3-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  background: #f0f0ff;
+}
+</style>
